d50c5e9aa4dded13f1538732c37b4889dac563a5
Raadwajeeh <rrq.wajeeh@gmail.com>
2025-12-10 10:29:57 +0100

    klaar

--- 2D.py
+++ 2D.py
@@ -1,5 +1,35 @@
 import turtle
 
-t = turtle.Turtle()
+screen = turtle.Screen()
+screen.title("Straight Track - Car Icon")
+screen.setup(width=533, height=800)
+screen.bgpic("track_straight.png")
 
-;klsdjfgit remote add origin https://github.com/RaadWajeeh/2D.git
+car = turtle.Turtle()
+car.penup()
+
+screen.addshape("car.gif")   
+car.shape("car.gif")      
+
+car.pensize(2)
+car.color("red")
+
+path = []
+y_start = -250
+y_end = 380
+step = 10
+
+y = y_start
+while y <= y_end:
+    path.append((0, y))
+    y += step
+
+car.goto(path[0][0], path[0][1])
+car.pendown()
+
+car.speed(2)
+
+for point in path[1:]:
+    car.goto(point[0], point[1])
+
+turtle.done()
